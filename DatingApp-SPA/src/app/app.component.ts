7aec3763b904a8bd14d18472ed69313398df6683
SK-BOBANL\Boban <bl@accent-skopje.mk>
2018-09-30 01:44:10 +0200

    End of section 6

--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -1,22 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-// import { AuthService } from './_services/auth.service';
-// import { JwtHelperService } from '@auth0/angular-jwt';
+import { AuthService } from './_services/auth.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title: 'app';
-  // jwtHelper = new JwtHelperService();
+export class AppComponent implements OnInit {
+  // title: 'app';
+  jwtHelper = new JwtHelperService();
 
-  // constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {}
 
-  // ngOnInit() {
-  //   const token = localStorage.getItem('token');
-  //   if (token) {
-  //     this.authService.decodedToken = this.jwtHelper.decodeToken(token);
-  //   }
-  // }
+  ngOnInit() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.authService.decodedToken = this.jwtHelper.decodeToken(token);
+    }
+  }
 }
